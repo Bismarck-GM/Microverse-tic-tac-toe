06695f44fb92ed8d9dffafd66f0e74e6c849288e
asiimwedismas <asiimwedismas@gmail.com>
2020-06-18 00:13:23 +0300

    Added logic to decide winner

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require './lib/player.rb'
 require './lib/board.rb'
+require './lib/the_judge.rb'
 
 puts 'Welcome to Tic-Tac-Toe.'
 
@@ -23,6 +24,8 @@ puts "Player one is: #{player_one.symbol} and Player two is: #{player_two.symbol
 
 counter = 0
 board = Board.new
+judge = Judge.new
+winner = nil
 
 loop do
   puts 'Player One. Is your turn. Please make a move (1-9):'
@@ -30,10 +33,18 @@ loop do
 
   counter += 1
   board.draw
-  break if counter == 9
+  winner = judge.is_winner?(board, player_one) ? 'player one' : nil
+  break if counter == 9 || !winner.nil?
 
   puts 'Player Two. Is your turn. Please make a move (1-9):'
   puts 'Invalid selection or occupied space' until player_two.make_move(gets.chomp.to_i, board)
   counter += 1
   board.draw
+  winner = judge.is_winner?(board, player_two) ? 'player one' : nil
+end
+
+if counter == 9
+  puts "It's a draw"
+else
+  puts winner
 end
