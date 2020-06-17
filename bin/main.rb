a4ff3e420d7239b396991b0d20490ae8f740d1ac
asiimwedismas <asiimwedismas@gmail.com>
2020-06-17 23:47:35 +0300

    Refactored code

--- bin/main.rb
+++ bin/main.rb
@@ -15,7 +15,7 @@ loop do
 end
 
 player_two = player_one == 'X' ? 'O' : 'X'
-              
+
 player_one = Player.new(player_one)
 player_two = Player.new(player_two)
 
@@ -26,46 +26,14 @@ board = Board.new
 
 loop do
   puts 'Player One. Is your turn. Please make a move (1-9):'
-  until player_one.make_move(gets.chomp.to_i, board) do
-    puts 'Invalid selection or occupied space'
-  end
+  puts 'Invalid selection or occupied space' until player_one.make_move(gets.chomp.to_i, board)
 
   counter += 1
   board.draw
   break if counter == 9
-  
+
   puts 'Player Two. Is your turn. Please make a move (1-9):'
-  until player_two.make_move(gets.chomp.to_i, board) do
-    puts 'Invalid selection or occupied space'
-  end
+  puts 'Invalid selection or occupied space' until player_two.make_move(gets.chomp.to_i, board)
   counter += 1
   board.draw
-
 end
-# counter = 0
-
-# loop do
-#   puts 'Player one. Is your turn. Please make a move (1-9):'
-
-#   player_one_selection = gets.chomp.to_i
-
-#   if player_one_selection >= 1 && player_one_selection <= 9
-#     puts "#{player_one} moves to #{player_one_selection}"
-#   else
-#     puts 'Invalid move.'
-#   end
-#   counter += 1
-#   break if counter == 9
-
-#   puts 'Player two. Is your turn. Please make a move (1-9):'
-
-#   player_two_selection = gets.chomp.to_i
-
-#   if player_two_selection >= 1 && player_two_selection <= 9
-#     puts "#{player_two} moves to #{player_two_selection}"
-#   else
-#     puts 'Invalid move.'
-#   end
-
-#   counter += 1
-# end
