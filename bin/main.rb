4d0896d9a7c822f726d1343bbb631fa1ee252d42
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-18 16:09:02 -0300

    Added function 'draw' from lib/board.rd to bin/main.rb. Re-factor it to work

--- bin/main.rb
+++ bin/main.rb
@@ -3,6 +3,12 @@ require './lib/player.rb'
 require './lib/board.rb'
 require './lib/the_judge.rb'
 
+def draw(board)
+  puts "#{board.cells[0]} #{board.cells[1]} #{board.cells[2]}"
+  puts "#{board.cells[3]} #{board.cells[4]} #{board.cells[5]}"
+  puts "#{board.cells[6]} #{board.cells[7]} #{board.cells[8]}"
+end
+
 puts 'Welcome to Tic-Tac-Toe.'
 
 puts 'The game is displayed on a 1-9 grid (top-to-bottom left-to-right).'
@@ -27,13 +33,13 @@ board = Board.new
 judge = Judge.new
 winner = nil
 
-board.draw
+draw(board)
 
 loop do
   puts 'Player One. Is your turn. Please make a move (1-9):'
   puts 'Invalid selection or occupied space' until player_one.make_move(gets.chomp.to_i, board)
 
-  board.draw
+  draw(board)
   counter += 1
   winner = judge.winner?(board, player_one) ? 'player one' : nil
   break if counter == 9 || !winner.nil?
@@ -41,7 +47,7 @@ loop do
   puts 'Player Two. Is your turn. Please make a move (1-9):'
   puts 'Invalid selection or occupied space' until player_two.make_move(gets.chomp.to_i, board)
 
-  board.draw
+  draw(board)
   counter += 1
   winner = judge.winner?(board, player_two) ? 'player two' : nil
   break unless winner.nil?
--- lib/board.rb
+++ lib/board.rb
@@ -12,9 +12,4 @@ class Board
     @cells[move_command - 1] = symbol
   end
 
-  def draw
-    puts "#{@cells[0]} #{@cells[1]} #{@cells[2]}"
-    puts "#{@cells[3]} #{@cells[4]} #{@cells[5]}"
-    puts "#{@cells[6]} #{@cells[7]} #{@cells[8]}"
-  end
 end
